2df9988510bde61d3feeb13424411553443f3880
mattiabenelli <benellimattia0@gmail.com>
2023-02-16 20:43:03 +0100

    struttura fatta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/js/app.js')
     <title>Document</title>
 </head>
 <body>
-    ciao
+    <header>
+        @include("partials.header")
+    </header>
+    <main>
+        @include("partials.main")
+    </main>
+    <footer>
+        @include("partials.footer")
+    </footer>
 </body>
 </html>
